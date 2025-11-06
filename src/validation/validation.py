e8a99ca26cd4f23e190e911290090df9eb9e843b
Krishnan Raghavan <krm9c@outlook.com>
2025-11-06 10:56:20 -0600

    Added code for average forgetting calculation, can be adapted by the validation team.

--- src/validation/validation.py
+++ src/validation/validation.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def test(model, loader, device, Graph=0):
+def test(model, loader, device):
     """
     Evaluate the model on the given dataset.
 
@@ -9,32 +9,24 @@ def test(model, loader, device, Graph=0):
     model (torch.nn.Module): The model to evaluate.
     loader (torch.utils.data.DataLoader): The dataset loader.
     Graph (int): Whether the data is a graph (0) or image (1). Defaults to 0.
+    
+    # KR: I am removing graph support for now, because, we want this to be 
+    # transferred to the model team eventually.
 
     Returns:
     float: The ratio of correct predictions.
-
     """
     model.eval()
     correct = 0
-    if Graph == 0:
-        for data in loader:  # Iterate in batches over the training/test dataset.
-            out = model(
-                data.x.float().to(device),
-                data.edge_index.to(device),
-                data.batch.to(device),
-            )
-            pred = out.argmax(dim=1)  # Use the class with highest probability.
-            correct += int(
-                (pred == data.y.to(device)).sum()
-            )  # Check against ground-truth labels.
-        return correct / len(loader.dataset)  # Derive ratio of correct predictions.
-    else:
-        for data in loader:  # Iterate in batches over the training/test dataset.
-            in_t, targets = data
-            in_t = in_t.unsqueeze(dim=1).float()
-            out = model(in_t.to(device))
-            pred = out.argmax(dim=1)  # Use the class with highest probability.
-            correct += int(
-                (pred == targets.to(device)).sum()
-            )  # Check against ground-truth labels.
-        return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+    for data in loader:  # Iterate in batches over the training/test dataset.
+        in_t, targets = data
+        in_t = in_t.unsqueeze(dim=1).float()
+        out = model(in_t.to(device))
+        pred = torch.max(out,1)[1]
+        correct += int(
+            (pred == targets.to(device)).sum()
+        )  # Check against ground-truth labels.
+        
+    print(correct, len(loader.dataset))
+    
+    return correct / len(loader.dataset)  # Derive ratio of correct predictions.
