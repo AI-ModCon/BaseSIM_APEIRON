99f60919dcb84bbcc21ba9fa32fe7f39f1f3f3c1
Steffen <steffen.schotthoefer@outlook.de>
2025-11-12 11:58:14 -0500

    fix mnist dataloader

--- examples/mnist/model.py
+++ examples/mnist/model.py
@@ -78,6 +78,7 @@ class MNIST_CNN(BaseModelHarness):
     def get_optmizer(self) -> Optimizer:
         return torch.optim.Adam(self.model.parameters(), lr=self.cfg.train.init_lr)
 
+    # --- Current task (MNIST with one new global drift) ---
     def get_cur_data_loaders(self) -> Tuple[DataLoader, DataLoader]:
         self._dispose_current_loaders()
 
