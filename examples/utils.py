d2b2788d0ff43d203c165c10e1f93f85527da56f
Ana Gainaru <ana.gainaru@gmail.com>
2026-02-27 09:06:21 -0500

    Make import statements for model classes conditional based on the dataset name

--- examples/utils.py
+++ examples/utils.py
@@ -1,17 +1,19 @@
-from examples.cifar.model import CIFAR_VISION
-from examples.imagenet.model import IMAGENET_VISION
-from examples.mnist.model import MNIST_CNN
-
 from config.configuration import Config
 from model.torch_model_harness import BaseModelHarness
 
 
 def get_example(cfg: Config) -> BaseModelHarness:
     if cfg.data.name == "mnist":
+        from examples.mnist.model import MNIST_CNN
+
         return MNIST_CNN(cfg=cfg)
     elif cfg.data.name == "cifar10":
+        from examples.cifar.model import CIFAR_VISION
+
         return CIFAR_VISION(cfg=cfg)
     elif cfg.data.name == "imagenet":
+        from examples.imagenet.model import IMAGENET_VISION
+
         return IMAGENET_VISION(cfg=cfg)
     else:
         raise NotImplementedError(
